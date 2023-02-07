b3d9e6ef9dc7cfb57985c9831451985c23dd156b
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-02-07 11:42:39 -0500

    Initial Commit

---
+++ StandardDeviationAndErrorCalculator/StandardDeviationAndErrorCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardDeviationAndErrorCalculatorApp.swift
+//  StandardDeviationAndErrorCalculator
+//
+//  Created by Graeme Armstrong on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct StandardDeviationAndErrorCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
