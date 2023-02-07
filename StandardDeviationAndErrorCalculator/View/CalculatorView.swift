1a9c2e7d4b0add8e7df25ae6f8594af6a5e6c4b1
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-02-07 11:58:47 -0500

    Deleted content view, made groups to show my data and created view file named CalculatorView

---
+++ StandardDeviationAndErrorCalculator/View/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  StandardDeviationAndErrorCalculator
+//
+//  Created by Graeme Armstrong on 2023-02-07.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
